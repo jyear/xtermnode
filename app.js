8e8ab92c20d6ac3855f81864c293eac81aaa2fd3
jyear <yx53074@hotmail.com>
2019-12-25 19:26:04 +0800

    sdsd

--- app.js
+++ app.js
@@ -97,6 +97,15 @@ router.post('/term', async (ctx, next) => {
   //返回启动的pid  用于socket连接后操作term
 
   //创建的时候 保存初始化terminal数据  以便socket连接后前端显示  并且判断初始化语句 以便判断语句执行完毕使用
+
+  // term.on('connection', () => {
+  //   ctx.response.body = {
+  //     data: term.pid.toString(),
+  //     code: 200,
+  //     message: 'success'
+  //   }
+  // })
+  await next()
   term.on('data', data => {
     logs[term.pid] += data
     if (!terms[parseInt(term.pid)].initCode) {
@@ -114,15 +123,6 @@ router.post('/term', async (ctx, next) => {
       }
     }
   })
-
-  // term.on('connection', () => {
-  //   ctx.response.body = {
-  //     data: term.pid.toString(),
-  //     code: 200,
-  //     message: 'success'
-  //   }
-  // })
-  await next()
 })
 app.use(router.routes())
 
